70f6481ac6e548a4bde21dc1c8ad743f094b56ee
Ricardo Alcobia <ricmanue@student.42lisboa.com>
2025-06-30 18:12:03 +0100

    adding classes

--- ex01/MyPhoneBook.hpp
+++ ex01/MyPhoneBook.hpp
@@ -1,14 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MyPhoneBook.cpp                                    :+:      :+:    :+:   */
+/*   MyPhoneBook.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/30 17:49:46 by ricmanue          #+#    #+#             */
-/*   Updated: 2025/06/30 17:54:15 by ricmanue         ###   ########.fr       */
+/*   Created: 2025/06/30 17:28:24 by ricmanue          #+#    #+#             */
+/*   Updated: 2025/06/30 17:57:55 by ricmanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "MyPhoneBook.hpp"
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
 
+#include <iostream>
+#include <string>
+
+
+# define SUCCESS 0
+# define UNSUCCESS -1
+
+class PhoneBook
+{
+
+};
+
+class Contact
+{
+
+};
+
+#endif
